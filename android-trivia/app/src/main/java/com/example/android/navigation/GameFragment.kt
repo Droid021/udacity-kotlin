0e739674a07dd8c30a4647841fabfbcdb4aaf51e
Droid <vincivenv@gmail.com>
2019-09-12 12:22:40 +0300

    Back stack manipulation and support for the up button

--- android-trivia/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ android-trivia/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
@@ -59,8 +60,7 @@ class GameFragment : Fragment() {
         binding.game = this
 
         // Set the onClickListener for the submitButton
-        binding.submitButton.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER")
-        { view: View ->
+        binding.submitButton.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER") { view: View ->
             val checkedId = binding.questionRadioGroup.checkedRadioButtonId
             // Do nothing if nothing is checked (id == -1)
             if (-1 != checkedId) {
@@ -80,10 +80,12 @@ class GameFragment : Fragment() {
                         setQuestion()
                         binding.invalidateAll()
                     } else {
-                        // We've won!  Navigate to the gameWonFragment.
+                        // game won
+                        view.findNavController().navigate(R.id.action_gameFragment_to_gameWonFragment)
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
+                    view.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment)
                 }
             }
         }
--- android-trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ android-trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
-
 
 class TitleFragment : Fragment() {
 
@@ -15,6 +15,15 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false
+        )
+//        binding.playButton.setOnClickListener { view: View ->
+//            //Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+
+//        using the navigation's on click listener
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
         return binding.root
     }
